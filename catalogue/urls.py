712c8df5f2660399f7c2790f58f5e9de8c39b1a5
Md Lutfor Tahman Shifat <shifat151@gmail.com>
2020-07-07 14:50:27 +0600

    book renew librarian functionality added

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     path('librarian/', views.LoanedBooksAllListView.as_view(), name='librarian'),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
 ]
